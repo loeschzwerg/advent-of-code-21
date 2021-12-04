594bf8eaac3b8103f61cab14b562634699689884
theloeschzwerg <tim.nebel@hhu.de>
2021-12-04 16:06:11 +0100

    add day 4

--- src/advent_of_code_21/utils.clj
+++ src/advent_of_code_21/utils.clj
@@ -19,3 +19,59 @@
        str/split-lines
        (mapv #(str/split % #" "))
        (mapv (fn [[k v]] [(keyword k) (edn/read-string v)]))))
+
+;; day 4
+(defn slurp-lines [file-name]
+  (->> file-name
+       slurp
+       str/split-lines))
+
+(defn read-vec
+  "a,b,... -> [a b ...]"
+  [string]()
+  (edn/read-string (format "[%s]" string)))
+
+(defn transpose [matrix]
+  (apply mapv vector matrix))
+
+;; general slurp
+(defn parse-tuple [a & tokens]
+  #_(prn '--> a tokens)
+  (let [first' (edn/read-string a)
+        toks   (if (= 1 (count tokens))
+                 (edn/read-string (first tokens))
+                 (mapv edn/read-string tokens))]
+    (cond
+      (not tokens)
+      first'
+      (symbol? first')
+      {first' toks}
+      :else
+      (cons first' toks))))
+
+(defn- parse-line [line]
+  #_('-> line)
+  (apply parse-tuple line))
+
+(defn- parse-part [part]
+  #_(prn '> part)
+  (->> part
+       (mapv #(str/trim %))
+       (mapv #(str/split % #"( +|,)"))
+       (mapv parse-line)))
+
+(defn slurp-input [file-name]
+  (let [file    (slurp file-name)
+        lines   (str/split-lines file)
+        parts   (partition-by empty? lines) ;; empty lines mark a break
+        cleaned (remove (comp empty? first) parts)] ;; remove empty parts
+    (if (= 1 (count cleaned))
+      (parse-part (first cleaned))
+      (mapv parse-part cleaned))))
+
+(comment
+  (slurp-input "resources/day_1.txt")
+  (slurp-input "resources/day_2.txt")
+  (slurp-input "resources/day_3.txt")
+  (clojure.pprint/pprint (slurp-input "resources/day_4.txt"))
+  )
--- test/advent_of_code_21/core_test.clj
+++ test/advent_of_code_21/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-21.day-1 :refer :all]
             [advent-of-code-21.day-2 :refer :all]
-            [advent-of-code-21.day-3 :refer :all]))
+            [advent-of-code-21.day-3 :refer :all]
+            [advent-of-code-21.day-4 :refer :all]))
 
 (deftest d1-test
   (testing "First day"
@@ -27,3 +28,27 @@
     (is (= oxygen (keep-common example max-key)))
     (is (= (* oxygen co2) (d3-two example)))
     (is (< 793616 (d3-two d3-data)))))
+
+(deftest d4-test
+  (let [example {:draws [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+                 :boards ['([14 21 17 24  4]
+                            [10 16 15  9 19]
+                            [18  8 23 26 20]
+                            [22 11 13  6  5]
+                            [ 2  0 12  3  7])
+                          '([ 3 15  0  2 22]
+                            [ 9 18 13 17  5]
+                            [19  8  7 25 23]
+                            [20 11 10 24  4]
+                            [14 21 16 12  6])]}]
+    (testing "Example input - day 4 part one" 
+      (is (= 4512 (d4-one example))))
+    (testing "Actual Input - day 4 part one"
+      (is (< 8424 (d4-one d4-data)))
+      (is (> 30240 (d4-one d4-data)))
+      (is (= 8442 (d4-one d4-data))))
+    
+    (testing "Example input - day 4 part two"
+      (is (= 1924 (d4-two example))))
+    (testing "Actual input - day 4 part two"
+      (is (= 4590 (d4-two d4-data))))))
